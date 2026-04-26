acff9d2335385d9e03c80f50494463376e1786dc
Alex2006ye <alexcarvalhoaraujobastos@gmail.com>
2026-04-26 12:43:34 -0300

    feat: arrumando p RetrofitService e também a API para garantir que o Login e o Cadastro sejam feitos corretamente

--- app/src/main/java/com/example/redesocial_bancodesangue/retrofit/UsuarioApi.java
+++ app/src/main/java/com/example/redesocial_bancodesangue/retrofit/UsuarioApi.java
@@ -1,13 +1,23 @@
 package com.example.redesocial_bancodesangue.retrofit;
 
 import com.example.redesocial_bancodesangue.dto.LoginDTO;
+import com.example.redesocial_bancodesangue.dto.UsuarioDoadorCreateDTO;
+import com.example.redesocial_bancodesangue.dto.UsuarioHemocentroCreateDTO;
 import com.example.redesocial_bancodesangue.model.Usuario;
+
+import java.util.Optional;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface UsuarioApi {
-    @POST("/users/login")
+    @POST("users/login")
     Call<Usuario> consultar(@Body LoginDTO dto);
+
+    @POST("users/salvarUsuarioDoador")
+    Call<Usuario> salvarUsuarioDoador(@Body UsuarioDoadorCreateDTO dto);
+
+    @POST("users/salvarUsuarioHemocentro")
+    Call<Usuario> salvarUsuarioHemocentro(@Body UsuarioHemocentroCreateDTO dto);
 }
