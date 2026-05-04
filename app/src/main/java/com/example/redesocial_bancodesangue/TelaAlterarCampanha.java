1ff6eea6d3da10bda118aa9b7015c26b94748297
Alex2006ye <alexcarvalhoaraujobastos@gmail.com>
2026-05-04 18:42:44 -0300

    feat: e claro essas telas criadas vem junto com essas classes de Java, ao qual também mexi para toda a lógica funcionar

---
+++ app/src/main/java/com/example/redesocial_bancodesangue/TelaAlterarCampanha.java
@@ -0,0 +1,133 @@
+package com.example.redesocial_bancodesangue;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.redesocial_bancodesangue.model.Campanha;
+import com.example.redesocial_bancodesangue.model.TipoSanguineo;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+public class TelaAlterarCampanha extends AppCompatActivity {
+
+    private EditText edtNomeCampanhaAlterar, edtDataInicioAlterar, edtDataFimAlterar;
+    private Button btnAlterarCampanha;
+    private int campoAtual = 0;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tela_alterar_campanha);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        edtDataFimAlterar = findViewById(R.id.edtDataFimAlterar);
+        edtDataInicioAlterar = findViewById(R.id.edtDataInicioAlterar);
+        edtNomeCampanhaAlterar = findViewById(R.id.edtNomeCampanhaAlterar);
+        btnAlterarCampanha = findViewById(R.id.btnAlterarCampanha);
+
+        edtDataFimAlterar.setText(getIntent().getStringExtra("dataFim"));
+        edtDataInicioAlterar.setText(getIntent().getStringExtra("dataInicio"));
+        edtNomeCampanhaAlterar.setText(getIntent().getStringExtra("nomeCampanha"));
+
+        Calendar calendar = Calendar.getInstance();
+
+        DatePickerDialog.OnDateSetListener dateSetListener = (view, year, month, dayOfMonth) -> {
+            // Cria um LocalDate com os valores escolhidos
+            LocalDate dataEscolhida = null;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                dataEscolhida = LocalDate.of(year, month + 1, dayOfMonth);
+            }
+            // Formata e exibe a data no EditText
+            DateTimeFormatter formatter = null;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if(campoAtual == 1)
+                    edtDataInicioAlterar.setText(dataEscolhida.format(formatter));
+                if(campoAtual == 2)
+                    edtDataFimAlterar.setText(dataEscolhida.format(formatter));
+            }
+            // Atualiza o objeto do seu modelo (ex: usuario.setDataNasc(dataEscolhida))
+        };
+
+        edtDataInicioAlterar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                campoAtual = 1;
+                new DatePickerDialog(
+                        TelaAlterarCampanha.this,
+                        dateSetListener,
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)
+                ).show();
+            }
+        });
+
+        edtDataFimAlterar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                campoAtual = 2;
+                new DatePickerDialog(
+                        TelaAlterarCampanha.this,
+                        dateSetListener,
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)
+                ).show();
+            }
+        });
+
+        btnAlterarCampanha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nomeCampanha = edtNomeCampanhaAlterar.getText().toString();
+                String dataInicio = edtDataInicioAlterar.getText().toString();
+                String dataFim = edtDataFimAlterar.getText().toString();
+
+                if(nomeCampanha.isEmpty() || dataInicio.isEmpty() || dataFim.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Campos vazios", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Campanha campanha = new Campanha();
+                campanha.setIdCampanha(getIntent().getIntExtra("idCampanha", -1));
+                campanha.setNomeCampanha(nomeCampanha);
+                campanha.setDataFim(dataFim);
+                campanha.setDataInicio(dataInicio);
+                campanha.setTipoSanguineoVisado(TipoSanguineo.valueOf(getIntent().getStringExtra("tipoSanguineo")));
+
+                Intent intent = new Intent(getApplicationContext(), TelaTipoSanguineoAlterar.class);
+                intent.putExtra("nomeCampanha", campanha.getNomeCampanha());
+                intent.putExtra("idCampanha", campanha.getIdCampanha());
+                intent.putExtra("dataInicio", campanha.getDataInicio());
+                intent.putExtra("dataFim", campanha.getDataFim());
+                intent.putExtra("tipoSanguineo", campanha.getTipoSanguineoVisado().name());
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/redesocial_bancodesangue/TelaCriarCampanhaTipoSanguineo.java
@@ -0,0 +1,119 @@
+package com.example.redesocial_bancodesangue;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.redesocial_bancodesangue.dto.CampanhaCreateDTO;
+import com.example.redesocial_bancodesangue.model.Campanha;
+import com.example.redesocial_bancodesangue.model.TipoSanguineo;
+import com.example.redesocial_bancodesangue.retrofit.CampanhaApi;
+import com.example.redesocial_bancodesangue.retrofit.RetrofitService;
+import com.example.redesocial_bancodesangue.retrofit.UsuarioApi;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class TelaCriarCampanhaTipoSanguineo extends AppCompatActivity {
+
+    private Button btnCriacaoCampanha;
+    private RadioButton radioApositivo, radioAnegativo, radioBpositivo, radioBnegativo, radioABpositivo,
+    radioABnegativo, radioOpositivo, radioOnegativo;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tela_criar_campanha_tipo_sanguineo);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnCriacaoCampanha = findViewById(R.id.btnFinalizarCriacaoCampanha);
+        radioApositivo = findViewById(R.id.radioApositivo);
+        radioAnegativo = findViewById(R.id.radioAnegativo);
+        radioBpositivo = findViewById(R.id.radioBpositivo);
+        radioBnegativo = findViewById(R.id.radioBnegativo);
+        radioABpositivo = findViewById(R.id.radioABpositivo);
+        radioABnegativo = findViewById(R.id.radioABnegativo);
+        radioOpositivo = findViewById(R.id.radioOpositivo);
+        radioOnegativo = findViewById(R.id.radioOnegativo);
+
+        RetrofitService retrofit = new RetrofitService();
+
+        CampanhaApi api = retrofit.getRetrofit().create(CampanhaApi.class);
+
+        btnCriacaoCampanha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Campanha campanha = new Campanha();
+                campanha.setNomeCampanha(getIntent().getStringExtra("nomeCampanha"));
+                campanha.setDataInicio(getIntent().getStringExtra("dataInicio"));
+                campanha.setDataFim(getIntent().getStringExtra("dataFim"));
+                campanha.setIdUsuarioHemocentro(getIntent().getIntExtra("idHemocentro", -1));
+
+                if(!radioApositivo.isChecked() && !radioAnegativo.isChecked() && !radioBpositivo.isChecked() && !radioBnegativo.isChecked() && !radioABpositivo.isChecked() && !radioABnegativo.isChecked() && !radioOpositivo.isChecked() && !radioOnegativo.isChecked()){
+                    Toast.makeText(getApplicationContext(), "É obrigatório selecionar um tipo sanguíneo como foco principal da campanha", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(radioApositivo.isChecked())
+                    campanha.setTipoSanguineoVisado(TipoSanguineo.APositivo);
+                if(radioAnegativo.isChecked())
+                    campanha.setTipoSanguineoVisado(TipoSanguineo.ANegativo);
+                if(radioBpositivo.isChecked())
+                    campanha.setTipoSanguineoVisado(TipoSanguineo.BPositivo);
+                if(radioBnegativo.isChecked())
+                    campanha.setTipoSanguineoVisado(TipoSanguineo.BNegativo);
+                if(radioABpositivo.isChecked())
+                    campanha.setTipoSanguineoVisado(TipoSanguineo.ABPositivo);
+                if(radioABnegativo.isChecked())
+                    campanha.setTipoSanguineoVisado(TipoSanguineo.ABNegativo);
+                if(radioOpositivo.isChecked())
+                    campanha.setTipoSanguineoVisado(TipoSanguineo.OPositivo);
+                if(radioOnegativo.isChecked())
+                    campanha.setTipoSanguineoVisado(TipoSanguineo.ONegativo);
+
+                CampanhaCreateDTO dto = new CampanhaCreateDTO(campanha.getNomeCampanha(), campanha.getDataInicio(),
+                        campanha.getDataFim(), campanha.getTipoSanguineoVisado(),
+                        campanha.getIdUsuarioHemocentro());
+
+                api.criarCampanha(dto).enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        if (response.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "Campanha criada com sucesso", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), TelaInicialHemocentros.class);
+                            intent.putExtra("idUsuario", campanha.getIdUsuarioHemocentro());
+                            startActivity(intent);
+                            finish();
+                        } else{
+                            Toast.makeText(getApplicationContext(), "Erro na criação da campanha", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable throwable) {
+                        Toast.makeText(getApplicationContext(), "Erro no salvamento da campanha " + throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+}
