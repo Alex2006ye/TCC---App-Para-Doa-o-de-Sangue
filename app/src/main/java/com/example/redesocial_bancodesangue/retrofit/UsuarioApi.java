0677a7e169c54863075a7792f2e08f5a29eb0315
Alex2006ye <alexcarvalhoaraujobastos@gmail.com>
2026-07-10 16:56:10 -0300

    feat: colocando o método de atualizar o token do usuário, que será ativado sempre que o usuário fazer o login

--- app/src/main/java/com/example/redesocial_bancodesangue/retrofit/UsuarioApi.java
+++ app/src/main/java/com/example/redesocial_bancodesangue/retrofit/UsuarioApi.java
@@ -1,6 +1,7 @@
 package com.example.redesocial_bancodesangue.retrofit;
 
 import com.example.redesocial_bancodesangue.dto.LoginDTO;
+import com.example.redesocial_bancodesangue.dto.UpdateTokenDTO;
 import com.example.redesocial_bancodesangue.dto.UsuarioDoadorCreateDTO;
 import com.example.redesocial_bancodesangue.dto.UsuarioHemocentroCreateDTO;
 import com.example.redesocial_bancodesangue.model.Usuario;
@@ -12,6 +13,7 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface UsuarioApi {
@@ -29,4 +31,7 @@ public interface UsuarioApi {
 
     @GET("users/hemocentros")
     Call<List<Usuario>> listarHemocentros();
+
+    @PUT("users/atualizarToken")
+    Call<Void> atualizarTokenFcm(@Body UpdateTokenDTO dto);
 }
